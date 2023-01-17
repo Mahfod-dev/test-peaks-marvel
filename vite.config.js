7b22e2a73cd9257fcbc1cfcbeb966789e9ff153a
Mahfod <mahfod.dev@gmail.com>
2023-01-17 17:25:53 +0100

    configuration pwa

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [
+    react(),
+    eslint(),
+    VitePWA({
+      registerType: "autoUpdate",
+    }),
+  ],
   test: {
     globals: true,
     environment: "jsdom",
